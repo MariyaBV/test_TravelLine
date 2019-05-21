d5c8259bad40122cf084f74d9c61b6f9430e91de
MariyaBV <mari.mv2008@gmail.com>
2019-05-21 15:50:43 +0300

    blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     //при загрузке стр заменяет цветной фон на img
-    BackgroundReplacement();
+    BackgroundReplacement('#body', 'b-body-background__background-img');
 });
 
-function BackgroundReplacement()
+function BackgroundReplacement(selector, className)
 {
-    var element = document.querySelector('#body');
-    element.classList.add('b-body-background__background-img');
+    var element = document.querySelector(selector);
+    element.classList.add(className);
+}
+
+function GetAllElementsByClass(id, className)
+{
+    document.getElementById(id).getElementsByClassName(className);
+}
+
+function RoomSelection()
+{
+    let allBlocks =  GetAllElementsByClass('roomsAndPrices', 'b-prices-rooms__block');
+    let allButtons = GetAllElementsByClass('roomsAndPrices', 'b-prices-rooms__button');
 }
